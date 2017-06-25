17af5a7741a962226e0e662c629cffebfe91850d
Patrice Lebel <patleb@users.noreply.github.com>
2017-06-25 18:54:05 -0400

    do not load ext_bundler more than once

--- lib/ext_bundler/bundler.rb
+++ lib/ext_bundler/bundler.rb
@@ -1,91 +1,97 @@
-module Bundler
-  class << self
-    attr_accessor :sourced_gems, :sourced_gems_computed
+unless defined?(Bundler::EXT_BUNDLER_LOADED)
+  module Bundler
+    class << self
+      attr_accessor :sourced_gems, :sourced_gems_computed
 
-    module WithSource
-      def gemfile_sourced_gems
-        @_gemfile_sourced_gems ||= root.join('Gemfile.sourced_gems')
-      end
-
-      def definition(unlock = nil)
-        super
-
-        unless sourced_gems_computed || !sourced_gems
-          @definition = Definition.build(default_gemfile, default_lockfile, unlock)
-          self.sourced_gems_computed = true
+      module WithSource
+        def gemfile_sourced_gems
+          @_gemfile_sourced_gems ||= root.join('Gemfile.sourced_gems')
         end
 
-        @definition
-      end
+        def definition(unlock = nil)
+          super
 
-      def default_gemfile
-        @_default_gemfile ||= begin
-          default_file = super
-          if ARGV[0] == 'update' && File.exist?(gemfile_sourced_gems)
-            update_file = app_config_path.join('Gemfile.ext_bundler')
-            FileUtils.copy(default_file, update_file)
-            File.open(update_file, 'a') do |f|
-              File.readlines(gemfile_sourced_gems).each do |line|
-                f.puts line
+          unless sourced_gems_computed || !sourced_gems
+            @definition = Definition.build(default_gemfile, default_lockfile, unlock)
+            self.sourced_gems_computed = true
+          end
+
+          @definition
+        end
+
+        def default_gemfile
+          @_default_gemfile ||= begin
+            default_file = super
+            if ARGV[0] == 'update' && File.exist?(gemfile_sourced_gems)
+              update_file = app_config_path.join('Gemfile.ext_bundler')
+              FileUtils.copy(default_file, update_file)
+              File.open(update_file, 'a') do |f|
+                File.readlines(gemfile_sourced_gems).each do |line|
+                  f.puts line
+                end
+              end
+              default_file = update_file
+            end
+            default_file
+          end
+        end
+      end
+      prepend WithSource
+    end
+
+    Dsl.class_eval do
+      class << self
+        module WithSource
+          def evaluate(gemfile, lockfile, unlock)
+            return super unless (paths = Bundler.sourced_gems)
+
+            File.open(Bundler.gemfile_sourced_gems, "w") do |f|
+              paths.each do |name, options|
+                options = options.each_with_object([]) do |(key, value), memo|
+                  memo << "#{key}: '#{value}'"
+                end
+                f.puts("gem '#{name}', #{options.join(', ')}")
               end
             end
-            default_file = update_file
+            builder = new
+            builder.eval_gemfile(gemfile)
+            builder.eval_gemfile(Bundler.gemfile_sourced_gems)
+            builder.to_definition(lockfile, unlock)
           end
-          default_file
         end
+        prepend WithSource
       end
     end
-    prepend WithSource
   end
 
-  Dsl.class_eval do
-    class << self
+  module Gem
+    Specification.class_eval do
       module WithSource
-        def evaluate(gemfile, lockfile, unlock)
-          return super unless (paths = Bundler.sourced_gems)
-
-          File.open(Bundler.gemfile_sourced_gems, "w") do |f|
-            paths.each do |name, options|
-              options = options.each_with_object([]) do |(key, value), memo|
-                memo << "#{key}: '#{value}'"
-              end
-              f.puts("gem '#{name}', #{options.join(', ')}")
-            end
+        def add_dependency(gem, *requirements)
+          options = requirements.last
+          options = if options.is_a?(Hash) && options.instance_of?(Hash)
+            requirements.pop
+          else
+            {}
           end
-          builder = new
-          builder.eval_gemfile(gemfile)
-          builder.eval_gemfile(Bundler.gemfile_sourced_gems)
-          builder.to_definition(lockfile, unlock)
+
+          if options.any?
+            Bundler.sourced_gems ||= {}
+            Bundler.sourced_gems[gem] = options
+          end
+
+          super
         end
       end
       prepend WithSource
     end
   end
-end
 
-module Gem
-  Specification.class_eval do
-    module WithSource
-      def add_dependency(gem, *requirements)
-        options = requirements.last
-        options = if options.is_a?(Hash) && options.instance_of?(Hash)
-          requirements.pop
-        else
-          {}
-        end
-
-        if options.any?
-          Bundler.sourced_gems ||= {}
-          Bundler.sourced_gems[gem] = options
-        end
-
-        super
-      end
-    end
-    prepend WithSource
+  if Bundler::VERSION < '2.0'
+    Bundler.settings["github.https"] = true
   end
-end
 
-if Bundler::VERSION < '2.0'
-  Bundler.settings["github.https"] = true
+  module Bundler
+    EXT_BUNDLER_LOADED = true
+  end
 end
