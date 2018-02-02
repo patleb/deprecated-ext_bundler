d191c6be7944e88bfc6bc212a638f2bc7b3b268d
Patrice Lebel <patleb@users.noreply.github.com>
2018-02-02 11:19:52 -0500

    correction for capistrano

--- lib/ext_bundler.rb
+++ lib/ext_bundler.rb
@@ -10,8 +10,10 @@ module ExtBundler
         end.map(&:first).each{ |gem| require gem.to_s }
       end
     else
-      @gem_dev_paths ||= File.readlines('.gem-dev').map(&:strip).compact
-      path = @gem_dev_paths.lazy.map{ |root| File.expand_path("#{root}/#{names}") }.find{ |path| Dir.exist? path }
+      if File.exist? '.gem-dev'
+        @gem_dev_paths ||= File.readlines('.gem-dev').map(&:strip).compact
+        path = @gem_dev_paths.lazy.map{ |root| File.expand_path("#{root}/#{names}") }.find{ |path| Dir.exist? path }
+      end
       @@gem_dev[names.to_sym] ? gem(names, path: path, require: false) : gem(names, *args)
       @@gem_dev[names.to_sym] = {
         require: (args.last.is_a?(Hash) ? args.last : {})[:require].nil?,
